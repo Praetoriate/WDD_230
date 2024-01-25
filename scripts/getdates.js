c9f66eb275a1bed43064b9a8907aba24f01439d1
Praetoriate <btravis.elm@gmail.com>
2024-01-24 22:07:03 -0800

    hamburger to homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,12 @@ document.querySelector("#year").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent = new Date(
   document.lastModified
 );
+
+//Hamburger fixins
+const hamButton = document.querySelector('#hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
