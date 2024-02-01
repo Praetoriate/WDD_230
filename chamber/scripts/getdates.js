0e9618d1ca8006037d0b9043739ce9c234a503b0
Praetoriate <btravis.elm@gmail.com>
2024-02-01 07:38:13 -0800

    chamber css 1

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,9 +2,16 @@
 document.querySelector("#year").textContent = new Date().getFullYear();
 
 //last modification
-document.querySelector("#lastModified").textContent = new Date(
-  document.lastModified
-);
+const last_update = new Date(document.lastModified);
+const formatted_date = last_update.toLocaleDateString("en-US", {
+	day: "numeric",
+	month: "2-digit",
+	year: "numeric",
+	hour: "numeric",
+	minute: "2-digit",
+	second: "2-digit"
+  });
+document.querySelector("#lastModified").textContent = formatted_date;
 
 //Hamburger fixins
 const hamButton = document.querySelector('#hamburger');
