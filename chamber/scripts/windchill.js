4deabb3f9bafbe25c2c52490df87b1fbc453b1fc
Praetoriate <btravis.elm@gmail.com>
2024-02-17 15:17:02 -0800

    let to const

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,6 @@
 //Calculate Windchill
-let temp = Number(document.querySelector('#temperature').textContent);
-let speed = Number(document.querySelector('#wind-speed').textContent);
+const temp = Number(document.querySelector('#temperature').textContent);
+const speed = Number(document.querySelector('#wind-speed').textContent);
 
 const chill = 35.74 + (0.6215 * temp) - (35.75 * (speed**0.16)) + (0.4275 * temp * (speed**0.16));
 
