b4f8782da341a20a08b7cda3200f7ed684aca7bc
Praetoriate <btravis.elm@gmail.com>
2024-02-21 20:03:21 -0800

    home page script seperate.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,11 +6,3 @@ document.querySelector("#lastModified").textContent = new Date(
   document.lastModified
 );
 
-//Hamburger fixins
-const hamButton = document.querySelector('#hamburger');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('show');
-	hamButton.classList.toggle('show');
-});
