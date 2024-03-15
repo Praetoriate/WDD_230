4be5589434c7439b7ff62c123c66b831b40e3b4a
Praetoriate <btravis.elm@gmail.com>
2024-03-15 14:48:41 -0700

    chamber home page update

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,19 @@
+const banner = document.getElementById("banner");
+const bannerButton = document.getElementById("banner-button")
+
+function showBanner() {
+    const today = new Date();
+  const day = today.getDay();
+
+  if (day === 1 || day === 2 || day === 5) {
+    banner.style.display = "flex";
+  } else {
+    banner.style.display = "none";
+  }
+}
+
+showBanner();
+
+bannerButton.addEventListener("click", function() {
+    banner.style.display = "none";
+  });
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,9 +1,22 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#current-phrase');
+
+const foreMinTemp1 = document.querySelector('#min-temp1');
+const foreMaxTemp1 = document.querySelector('#max-temp1');
+const forePhrase1 = document.querySelector('#phrase1');
+
+const foreMinTemp2 = document.querySelector('#min-temp2');
+const foreMaxTemp2 = document.querySelector('#max-temp2');
+const forePhrase2 = document.querySelector('#phrase2');
+
+const foreMinTemp3 = document.querySelector('#min-temp3');
+const foreMaxTemp3 = document.querySelector('#max-temp3');
+const forePhrase3 = document.querySelector('#phrase3');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.522&lon=-122.0139&appid=7a0ebb7a38cc64dbc0a1d88697c39535&units=imperial';
-const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.522&lon=-122.0139&exclude=hourly,minutely&appid=7a0ebb7a38cc64dbc0a1d88697c39535&units=imperial';
+// const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.522&lon=-122.0139&exclude=hourly,minutely&appid=7a0ebb7a38cc64dbc0a1d88697c39535&units=imperial';
+const forecastUrl = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/2155444?apikey=fcHEQHxUxACs3b6E0LCcBGn0ru7Eh0xG';
 
 async function apiFetch() {
     try {
@@ -12,14 +25,14 @@ async function apiFetch() {
         if (answer.ok) {
             const data = await answer.json();
             console.log(data);
-        displayResults(data);
+        displayCurrent(data);
         } else {
             throw Error(await answer.text());
         }
         if (answer2.ok) {
             const data2 = await answer2.json();
             console.log(data2);
-        // displayResults(data);
+        displayForecast(data2);
         } else {
             throw Error(await answer2.text());
         }
@@ -30,11 +43,25 @@ async function apiFetch() {
 
 apiFetch();
  
-function displayResults(data) {
+function displayCurrent(data) {
     currentTemp.innerHTML = `${data.main.temp} &deg;F`
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc}`;
+}
+
+function displayForecast(data2) {
+    foreMinTemp1.innerHTML = `${data2.DailyForecasts[1].Temperature.Minimum.Value} &deg;F`
+    foreMaxTemp1.innerHTML = `${data2.DailyForecasts[1].Temperature.Maximum.Value} &deg;F`
+    forePhrase1.innerHTML = `${data2.DailyForecasts[1].Day.IconPhrase}`
+
+    foreMinTemp2.innerHTML = `${data2.DailyForecasts[2].Temperature.Minimum.Value} &deg;F`
+    foreMaxTemp2.innerHTML = `${data2.DailyForecasts[2].Temperature.Maximum.Value} &deg;F`
+    forePhrase2.innerHTML = `${data2.DailyForecasts[2].Day.IconPhrase}`
+    
+    foreMinTemp3.innerHTML = `${data2.DailyForecasts[3].Temperature.Minimum.Value} &deg;F`
+    foreMaxTemp3.innerHTML = `${data2.DailyForecasts[3].Temperature.Maximum.Value} &deg;F`
+    forePhrase3.innerHTML = `${data2.DailyForecasts[3].Day.IconPhrase}`
 }
